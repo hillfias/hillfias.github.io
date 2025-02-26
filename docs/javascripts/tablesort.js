ad1459defa186d9d67ca632b4253d944dc005e59
William Fuchs <fuchs.william@hotmail.fr>
2025-02-26 01:14:58 +0100

    clean css for footer. add js trick for mobile.

--- docs/javascripts/tablesort.js
+++ docs/javascripts/tablesort.js
@@ -1,6 +1,10 @@
 document$.subscribe(function() {
-    var tables = document.querySelectorAll("article table:not([class])")
-    tables.forEach(function(table) {
-      new Tablesort(table)
-    })
+  var tables = document.querySelectorAll("article table:not([class])")
+  tables.forEach(function(table) {
+    new Tablesort(table)
   })
+})
+
+window.onload = function() {
+  document.body.addEventListener('touchstart', function() {}, false);
+}
